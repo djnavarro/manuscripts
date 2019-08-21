19a3d5f5f3a807671ccb9b6d68bad45b188c71ba
Danielle Navarro <d.navarro@unsw.edu.au>
2019-08-21 14:36:29 +1000

    update for 2016

--- R/arrange_bib.R
+++ R/arrange_bib.R
@@ -2,9 +2,10 @@ library(dplyr)
 library(here)
 library(bib2df)
 
-bib <- here("citation", "danielle.bib")
+bib_file <- here("danielle.bib")
 
-bib %>%
+bib_df <- bib %>%
   bib2df() %>%
-  arrange(desc(YEAR), BIBTEXKEY) %>%
-  df2bib(file = bib)
+  arrange(desc(YEAR), BIBTEXKEY)
+
+# df2bib(bib_df, file = bib_file)
